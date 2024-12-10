5e61b9e7f6f4ba48252fadb7394f3c8b0b143ac2
Elisa Delannoy <elisadelannoy10@gmail.com>
2024-12-10 17:29:45 +0100

    erroronprocess

--- push_swap.c
+++ push_swap.c
@@ -12,13 +12,35 @@
 
 #include "libft/libft.h"
 #include "printf/ft_printf.h"
+#include "push_swap.h"
+
+int	ft_error(int argc, char **argv)
+{
+	int		i;
+	int		nb;
+	char	*nbcheck;
+
+	i = 1;
+	while (i < argc)
+	{
+		nb = ft_atoi(argv[i]);
+		nbcheck = ft_itoa(nb);
+		if (ft_strcmp(nbcheck, argv[i]) != 0)
+		{
+			ft_printf("Error\n");
+			exit(1);
+		}
+		i++;
+	}
+	return (nb);
+}
 
 t_list	*ft_addlst(char *lst, int i)
 {
 	t_list *a = NULL;
 	t_list *newa = NULL;
 	t_list *temp;
-
+	
 	if (i == 1)
 		a = ft_lstnew(lst);
 	else
@@ -46,6 +68,7 @@ int main(int argc, char **argv)
 	i = 1;
 	if (argc > 1)
 	{
+		ft_error(argc, argv);
 		while (argv[i])
 		{
 			a = ft_addlst(argv[i], i);
